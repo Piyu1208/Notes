ab9fc71b10b7e3feed459941843693a79ddf56dd
Piyush Sharma <piyu22551@gmail.com>
2026-02-09 19:44:00 +0530

    refresh route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { signUp, login } from '../controllers/authController.js';
+import { signUp, login, refreshAccessToken } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 
 router.post('/login', login);
+
+router.post('/refresh', refreshAccessToken);
 
 
 export default router;
