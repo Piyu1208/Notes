7f17913ccfb4e2d1233e1d12bd3937907e5847da
Piyush Sharma <piyu22551@gmail.com>
2026-02-20 19:07:17 +0530

    displayed notes

--- frontend/my_app/src/pages/Home.jsx
+++ frontend/my_app/src/pages/Home.jsx
@@ -1,10 +1,62 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import api from '../utils/axios.js';
 
 function Home() {
+    const [notes, setNotes] = useState([]);
+
+    const formatDate = (dateString) => {
+        return new Date(dateString).toLocaleString();
+    };
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const res = await api.get("/api/notes", {withCredentials: true});
+                setNotes(res.data.data);
+                console.log(res.data);
+            } catch (err) {
+                console.error("Failed to load notes");
+            }
+        }
+
+        fetchTasks();
+    }, []);
 
     return (
-        <h2>All Notes</h2>
+        <div style={styles.container}>
+            <h2>Your Notes</h2>
+
+            {notes.length === 0 ? (
+                <p>No notes yet</p>
+            ) : (
+                <div style={styles.grid}>
+                    {notes.map((note) => (
+                        <div key={note.id} style={styles.card}>
+                            <h4>{note.title}</h4>
+                            <small>{formatDate(note.updated_at)}</small>
+                        </div>
+                    ))}
+                </div>
+            )}
+
+        </div>
     );
 }
+
+const styles = {
+  container: {
+    padding: "24px",
+  },
+  grid: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+    gap: "16px",
+  },
+  card: {
+    border: "1px solid #ddd",
+    padding: "16px",
+    borderRadius: "8px",
+  },
+};
 
 export default Home;
