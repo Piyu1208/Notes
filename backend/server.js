aa8a63dcb25880f4392921e61a4112505eb4bd2e
Piyush Sharma <piyu22551@gmail.com>
2026-02-24 14:42:14 +0530

    added port

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,10 @@ app.use('/api', userRoutes);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 
