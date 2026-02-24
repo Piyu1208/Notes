7ce0c12519e7d703abe432d3e6c295fcbc9c304c
Piyush Sharma <piyu22551@gmail.com>
2026-02-24 17:07:31 +0530

    configured cookies

--- backend/utils/cookieOptions.js
+++ backend/utils/cookieOptions.js
@@ -2,12 +2,14 @@ export const refreshCookieOptions = {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
-    maxAge: 30 * 24 * 60 * 60 * 1000
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: '/',
 };
 
 export const accessCookieOptions = {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
-    maxAge: 15 * 60 * 60 * 1000
+    maxAge: 15 * 60 * 60 * 1000,
+    path: '/',
 };
